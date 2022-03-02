e87b655dc9b47e7407e31dc360c970f49b372e78
leighfall <autumn.leigh.henderson@gmail.com>
2022-03-01 20:21:44 -0500

    feat: Add Navigation Component #29

--- adversarial/src/App.vue
+++ adversarial/src/App.vue
@@ -1,28 +1,40 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="main-container">
+      <Navigation pageTitle="Home"/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navigation from './components/Navigation.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navigation
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @font-face {
+    font-family: "Raleway";
+    src: url(./fonts/raleway/Raleway[wght].ttf) format("truetype");
+  }
+  @font-face {
+    font-family: "Raleway-italic";
+    src: url(./fonts/raleway/Raleway-Italic[wght].ttf) format("truetype");
+  }
+  @font-face {
+    font-family: "Montserrat";
+    src: url(./fonts/Montserrat/Montserrat-VariableFont_wght.ttf) format("truetype");
+  }
+
+  body {
+    margin: 0;
+  }
+
+  .main-container {
+    width: 100%;
+    margin: 0 auto;
+  }
 </style>
---
+++ adversarial/src/components/Navigation.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="banner">
+    <div class="headers">Adversarial Learning</div>
+    <div class="headers page-title">-</div>
+    <div class="headers page-title">{{ pageTitle }}</div>
+    <div class="links">
+      <a href="#">Home</a>
+      <a href="#">FAQ</a>
+      <a href="#">Demo</a>
+      <a href="#">API</a>
+      <a href="#">GitHub</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+  props: {
+    pageTitle: String
+  }
+}
+</script>
+
+<style>
+.banner {
+  display: flex;
+  width: 100%;
+  height: 100px;
+  background-color: #D3DAE4;
+  padding-left: 35px;
+}
+
+.headers {
+  display: inline-block;
+  color: #333333;
+  font-weight: bolder;
+  font-family: "Raleway-italic";
+  font-size: 28pt;
+  margin-top: 25px;
+}
+
+.page-title {
+  margin-left: 10px;
+}
+
+.links {
+  display: inline-block;
+  margin-top: 40px;
+  margin-left: auto;
+  margin-right: 35px;
+}
+
+a {
+  color: #333333;
+  font-family: "Montserrat";
+  text-decoration: none;
+  margin-right: 35px;
+}
+
+a:hover {
+  color: #0C68BE;
+}
+
+a:active {
+  font-weight: bold;
+}
+</style>
+
