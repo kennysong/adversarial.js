2b113d01ede5881558f5edf04ff6ec09223ca3e8
andyv0110 <72678120+andyv0110@users.noreply.github.com>
2022-03-27 16:24:09 -0400

    text changes

--- adversarial/src/components/ModelDropDown.vue
+++ adversarial/src/components/ModelDropDown.vue
@@ -1,21 +1,27 @@
 <template>
   <div class="dropdown">
-  <select id="select-model">
-            <option value="" selected disabled>Select model</option>
-            <option value="mnist">MNIST (digit recognition)</option>
-            <option value="gtsrb">GTSRB (street sign recognition)</option>
-            <option value="cifar">CIFAR-10 (object recognition, small)</option>
-            <option value="imagenet">ImageNet (object recognition, large)</option>
-          </select>
-  </div>
-</template>
-
+      <button class="btn btn-primary dropdown-toggle" type="button" id="ModelDropDown" data-bs-toggle="dropdown" aria-expanded="false">
+      {{value}}
+      </button>
+        <ul class="dropdown-menu" aria-labelledby="ModelDropDown">
+        <li v-for="option in options" :key="option">
+          <a class="dropdown-item" @click="value = option" href="javascript: void(0)">{{option}}</a>
+        </li>
+        </ul>
+      </div>
+  </template>
 <script>
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
 export default {
   name: 'ModelDropDown',
   props: {
     description: String
-  }
+  },
+  data: () => ({
+    options: ['MNIST (digit recognition)', 'GTSRB (street sign recognition)', 'CIFAR-10 (object recognition, small)', 'ImageNet (object recognition, large)'],
+    value: 'Select Model'
+  }),
 }
 </script>
 
